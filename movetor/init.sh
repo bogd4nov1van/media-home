c0036c1270c58d9e97448bc2652f3e8e1549a996
ivanbogdanov <bogdanov.i@medlinx.online>
2022-09-16 18:09:11 +0300

    movetor

---
+++ movetor/init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p $1/torrent
+mkdir -p $1/torrent/movies
+mkdir -p $1/torrent/serials
+mkdir -p $1/torrent/music
+mkdir -p $1/torrent/samples
---
+++ movetor/movetor.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import os
+
+torrent_base_dir='/Users/ivanbogdanov/Downloads/torrent/'
+dest_base_dir='nzo@192.168.31.100:/mnt/toshiba/torrent/'
+
+print('start scipt in: ' + torrent_base_dir)
+
+for _, tor_types, _ in os.walk(torrent_base_dir):
+    for tor_type in tor_types:
+
+        type_path=torrent_base_dir + tor_type
+        print('find torrent in: ' + type_path)
+    
+        for _, _, tor_files in os.walk(type_path):
+            for file_name in tor_files:
+
+                tor_path = '\'' + type_path + '/' + file_name + '\''
+                dest_path = dest_base_dir + tor_type
+                
+                scp_command='scp ' + tor_path + ' ' + dest_path
+                clear_command='rm -f ' + tor_path
+                
+                print('run: ' + scp_command)
+                result = os.system(scp_command)
+
+                if(result == 0):
+                    print('run: ' + clear_command)
+                    os.system(clear_command)
+
+print('end scipt')
